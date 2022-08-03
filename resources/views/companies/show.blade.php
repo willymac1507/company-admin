a75f7338fd702a728d15cc0e5f2b35ba2b91dd25
Will McCloy <william.mccloy@googlemail.com>
2022-08-03 14:53:59 +0100

    Add logo and edit forms

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -10,11 +10,11 @@
 
         </div>
         <div class="card-body">
-            <div class="row h-25 align-content-center">
+            <div class="row">
                 <div class="col-10">
-                    <h3 class="card-title mb-0">{{ $company->name }}</h3>
+                    <h3 class="card-title mt-2">{{ $company->name }}</h3>
                 </div>
-                <div class="col-2">
+                <div class="col-2 text-end">
                     <img src="/storage/{{ $company->logo }}" alt="logo" width="50">
                 </div>
             </div>
